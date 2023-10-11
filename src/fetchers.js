cf6a2d4e19e09d775919647afe510938d68216d2
Tim Ji <timji@kktv.me>
2023-10-11 08:06:56 +0800

    add camelize

--- src/fetchers.js
+++ src/fetchers.js
@@ -1,6 +1,9 @@
+import camelize from 'camelize';
+// import axios from 'axios';
+
 export const listFetcher = (url) => {
   if (url === '/following_list') {
-    return Promise.resolve([
+    return Promise.resolve(camelize([
       {
         'title': 'Audi_A4_S4',
         'cover': '/images/Audi_A4_S4.png',
@@ -16,10 +19,9 @@ export const listFetcher = (url) => {
         'cover': '/images/Range_Rover_Sport_L322.png',
         'play_url': '/media/Range_Rover_Sport_L322.m3u8'
       }
-    ]);
-
+    ]));
   } else if (url === '/for_you_list') {
-    return Promise.resolve([
+    return Promise.resolve(camelize([
       {
         'title': 'Rolls_Royce_Ghost',
         'cover': '/images/Rolls_Royce_Ghost.png',
@@ -35,7 +37,8 @@ export const listFetcher = (url) => {
         'cover': '/images/Volkswagen_Golf_7.png',
         'play_url': '/media/Volkswagen_Golf_7.m3u8'
       }
-    ]);
+    ]));
   }
+  // return axios.get('http://localhost:3001'+url)
   return Promise.reject(new Error('Cannot GET ' + url));
 }
